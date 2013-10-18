ed7846215c8d26539768adeae437f8dee1490c45
Henry AT <yiperu@gmail.com>
2013-10-18 14:16:05 -0300

    Se actualizo la codificacino para que el menu pueda funcionar correctamente, now operativo

--- ProyectoMenuBasicoAndroidProject/ProyectoMenuBasicoAndroid/src/main/java/com/apps4s/proyectomenubasicoandroid/MainActivity.java
+++ ProyectoMenuBasicoAndroidProject/ProyectoMenuBasicoAndroid/src/main/java/com/apps4s/proyectomenubasicoandroid/MainActivity.java
@@ -1,8 +1,11 @@
 package com.apps4s.proyectomenubasicoandroid;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -19,5 +22,25 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        AlertDialog alerta = new AlertDialog.Builder(this).create();
+        alerta.setTitle(getString(R.string.tituloAlerta));
+        //alerta.setButton("OK""Nada");
+
+        switch (item.getItemId()) {
+            case R.id.m_inicio:
+                alerta.setMessage(getString(R.string.m_inicio));
+                break;
+            case R.id.m_editar:
+                alerta.setMessage(getString(R.string.m_editar));
+                break;
+            case R.id.m_about:
+                alerta.setMessage(getString(R.string.m_about));
+                break;
+        }
+        alerta.show();
+        return false;
+    }
 }
